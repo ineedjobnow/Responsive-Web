71c3402e0ac24ff4396baccb62a9805613d76588
ineedjobnow <119470979+ineedjobnow@users.noreply.github.com>
2023-01-29 13:58:52 +0900

    Update custom.js

--- bootstrap/web01/js/custom.js
+++ bootstrap/web01/js/custom.js
@@ -68,7 +68,7 @@ $(function(){
    
    
 })
-/*
+
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
 	center: new kakao.maps.LatLng(37.6447043, 126.6670912), //지도의 중심좌표.
@@ -84,4 +84,4 @@ var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리
 
 // 마커가 지도 위에 표시되도록 설정합니다
 marker.setMap(map);
-*/
+
